5d8ba4e122d40f5e9dc175879f1c5e626abc7beb
Emre <E22536@eon.com>
2022-07-25 16:08:46 +0200

    ...

--- src/components/section.js
+++ src/components/section.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./styles.css";
 
 const Section = ({ title, subtitle, dark, id }) => {
   return (
-    <div className={"section" + (dark ? "section-dark" : "")}>
-      <div className="section-content" id={id}>
-        <h1 className="text-2xl">{title}</h1>
-        <p>{subtitle}</p>
+    <div className="p-4 ">
+      <div className={"section" + (dark ? "section-dark" : "")}>
+        <div className="section-content" id={id}>
+          <h1 className="text-2xl">{title}</h1>
+          <p className="text-start">{subtitle}</p>
+        </div>
       </div>
     </div>
   );

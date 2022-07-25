58209a16609857e03c064c3f5dfd46dbb67e319f
Emre <E22536@eon.com>
2022-07-25 10:59:43 +0200

    create

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import Homepage from "./page";
 import "./App.css";
 
 function App() {
---
+++ src/page/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "../components/navigation";
+import Section from "../components/section";
+
+const Homepage = () => {
+  return <div></div>;
+};
+
+export default Homepage;
